bcdadc1fb84b8f56a4eee7bb4360470e061cf70c
Clarance Liberiste Ntwari <60586899+claranceliberi@users.noreply.github.com>
2024-05-18 23:32:32 +0200

    feat: make project folder and pull changes

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
+	"runtime/debug"
 
 	"github.com/gin-gonic/gin"
 )
@@ -28,69 +29,96 @@ func main() {
 }
 
 func handleWebhook(c *gin.Context) {
-	var payload map[string]interface{}
+	var payload map[string]any
 
 	if err := c.ShouldBindJSON(&payload); err != nil {
-		fmt.Println("Error occured %+v\n", err.Error())
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+
+		returnError(c, err.Error())
 	}
+
+	// fmt.Println("Got Payload %d", payload)
 
 	// get event type
-	eventType, err := extractEventType(c, payload)
+	event, err := extractEventType(c, payload)
 
 	if err != nil {
-		c.JSON(http.StatusAccepted, gin.H{
-			"message": err.Error(),
-		})
+		returnError(c, err.Error())
 		return
 	}
 
-	repository, ok := payload["repository"].(map[string]interface{})
-	if !ok {
-		c.JSON(http.StatusAccepted, gin.H{
-			"message": "missing repository data",
+	nameAction := event.nameAction
+
+	isHandledEVentAction := nameAction == "workflow_run.completed" || nameAction == "pull_request.closed" || nameAction == "pull_request.opened" || nameAction == "pull_request.reopened"
+
+	if isHandledEVentAction {
+		repository, ok := payload["repository"].(map[string]any)
+		if !ok {
+			returnError(c, err.Error())
+			return
+		}
+
+		repositoryName, ok := repository["name"].(string)
+		if !ok {
+			returnError(c, err.Error())
+			return
+		}
+
+		repositoryAddress, ok := repository["html_url"].(string)
+		if !ok {
+			returnError(c, err.Error())
+			return
+		}
+
+		sshAddress, ok := repository["ssh_url"].(string)
+		if !ok {
+			returnError(c, err.Error())
+			return
+		}
+
+		branchName, err := extractBranchName(event, payload)
+		if err != nil {
+			returnError(c, err.Error())
+			return
+		}
+
+		prId, err := extractPRID(event, payload)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"message": err.Error(),
+			})
+			return
+		}
+
+		prNumber, err := extractPRNumber(event, payload)
+		if err != nil {
+			returnError(c, err.Error())
+			return
+		}
+
+		PR := MyPullRequest{
+			branchName:  branchName,
+			repoName:    repositoryName,
+			repoAddress: repositoryAddress,
+			sshAddress:  sshAddress,
+			prID:        prId,
+			prNumber:    prNumber,
+		}
+
+		err = PR.PullChanges()
+
+		if err != nil {
+			returnError(c, err.Error())
+		}
+		c.JSON(http.StatusCreated, gin.H{
+			"message": "not yet started",
 		})
 		return
 	}
-
-	repositoryName, ok := repository["name"].(string)
-	if !ok {
-		c.JSON(http.StatusAccepted, gin.H{
-			"message": "missing repository name",
-		})
-		return
-	}
-
-	repositoryAddress, ok := repository["html_url"].(string)
-	if !ok {
-		c.JSON(http.StatusAccepted, gin.H{
-			"message": "missing repository address",
-		})
-		return
-	}
-
-	sshAddress, ok := repository["ssh_url"].(string)
-	if !ok {
-		c.JSON(http.StatusAccepted, gin.H{
-			"message": "missing ssh address",
-		})
-		return
-	}
-
-	fmt.Println("Payload: %+v\n", payload)
 
 	c.JSON(http.StatusAccepted, gin.H{
 		"message": "not yet started",
 	})
 }
-
-// type MyPullRequest struct {
-// 	branchName  string
-// 	repoName    string
-// 	repoAddress string
-// 	sshAddress  string
-// 	prID        string
-// }
 
 type Event struct {
 	name       string
@@ -98,7 +126,7 @@ type Event struct {
 	nameAction string
 }
 
-func extractEventType(c *gin.Context, payload map[string]interface{}) (Event, error) {
+func extractEventType(c *gin.Context, payload map[string]any) (Event, error) {
 	// Get the X-GitHub-Event header
 	eventType := c.GetHeader("X-GitHub-Event")
 
@@ -119,6 +147,124 @@ func extractEventType(c *gin.Context, payload map[string]interface{}) (Event, er
 
 	return event, nil
 }
+func extractBranchName(event Event, payload map[string]any) (string, error) {
+	name := event.name
+	var branchName string
+
+	if name == "pull_request" {
+		pullRequest, ok := payload["pull_request"].(map[string]any)
+		if !ok {
+			return "", errors.New(event.nameAction + " - Could not extract branch name from pull_request - no pull_request :" + fmt.Sprintf("%v", pullRequest))
+		}
+
+		head, ok := pullRequest["head"].(map[string]any)
+		if !ok {
+			return "", errors.New(event.nameAction + " - Could not extract branch name - no head :" + fmt.Sprintf("%v", head))
+		}
+
+		branchName, _ = head["ref"].(string)
+	}
+
+	if name == "workflow_run" {
+		workflowRun, ok := payload["workflow_run"].(map[string]any)
+
+		if !ok {
+			return "", errors.New(event.nameAction + " - Could not extract branch name from workflow_run - no workflow_run :" + fmt.Sprintf("%v", workflowRun))
+		}
+		pullRequests, ok := workflowRun["pull_requests"].([]interface{})
+		if !ok || len(pullRequests) == 0 {
+			return "", errors.New(event.nameAction + " - Could not extract branch name from workflow_run - no pull_requests :" + fmt.Sprintf("%v", pullRequests))
+
+		}
+
+		head, ok := pullRequests[0].(map[string]any)["head"].(map[string]any)
+		if !ok {
+			return "", errors.New(event.nameAction + " - Could not extract branch name from workflow_run - no head :" + fmt.Sprintf("%v", head))
+		}
+		branchName, _ = head["ref"].(string)
+	}
+
+	return branchName, nil
+}
+
+func extractPRID(event Event, payload map[string]any) (float64, error) {
+	name := event.name
+	var prId float64
+	errorMessage := errors.New(event.nameAction + " - Could not extract PR ID")
+
+	if name == "pull_request" {
+		pullRequest, ok := payload["pull_request"].(map[string]any)
+		if !ok {
+			return 0, errorMessage
+		}
+
+		prId, _ = pullRequest["id"].(float64)
+	}
+
+	if name == "workflow_run" {
+		workflowRun, ok := payload["workflow_run"].(map[string]any)
+		if !ok {
+			return 0, errorMessage
+		}
+		pullRequests, ok := workflowRun["pull_requests"].([]interface{})
+		if !ok || len(pullRequests) == 0 {
+			return 0, errorMessage
+
+		}
+
+		pullRequest, ok := pullRequests[0].(map[string]any)
+		if !ok {
+			return 0, errorMessage
+		}
+		prId, _ = pullRequest["id"].(float64)
+	}
+
+	return prId, nil
+}
+
+func extractPRNumber(event Event, payload map[string]any) (float64, error) {
+	name := event.name
+	var prNumber float64
+	errorMessage := errors.New(event.nameAction + " - Could not extract PR Number")
+
+	if name == "pull_request" {
+		pullRequest, ok := payload["pull_request"].(map[string]any)
+		if !ok {
+			return 0, errorMessage
+		}
+
+		prNumber, _ = pullRequest["number"].(float64)
+	}
+
+	if name == "workflow_run" {
+		workflowRun, ok := payload["workflow_run"].(map[string]any)
+		if !ok {
+			return 0, errorMessage
+		}
+		pullRequests, ok := workflowRun["pull_requests"].([]interface{})
+		if !ok || len(pullRequests) == 0 {
+			return 0, errorMessage
+
+		}
+
+		pullRequest, ok := pullRequests[0].(map[string]any)
+		if !ok {
+			return 0, errorMessage
+		}
+		prNumber, _ = pullRequest["number"].(float64)
+	}
+
+	return prNumber, nil
+}
+
+func returnError(c *gin.Context, message string) {
+	c.JSON(http.StatusBadRequest, gin.H{
+		"message": message,
+	})
+	debug.PrintStack()
+	fmt.Println("Error occured %+v\n", message)
+
+}
 
 // function that receives repoaddress, branchname
 // clone the branch from the repo
@@ -135,7 +281,8 @@ type PullRequest interface {
 	GetRepoName() string
 	GetRepoAddress() string
 	GetSSHAddress() string
-	GetPRID() string
+	GetPRID() float64
+	GetPrNumber() float64
 	PrepareDir() (string, error)
 	PullChanges() error
 	CommunicateProgress(status string) error
@@ -157,7 +304,8 @@ type MyPullRequest struct {
 	repoName    string
 	repoAddress string
 	sshAddress  string
-	prID        string
+	prID        float64
+	prNumber    float64
 }
 
 func (pr *MyPullRequest) GetBranchName() string {
@@ -175,11 +323,18 @@ func (pr *MyPullRequest) GetSSHAddress() string {
 	return pr.sshAddress
 }
 
-func (pr *MyPullRequest) GetPRID() string {
-	return pr.repoAddress
+func (pr *MyPullRequest) GetPRID() float64 {
+	return pr.prID
 }
+
+func (pr *MyPullRequest) GetPrNumber() float64 {
+	return pr.prNumber
+}
+
 func (pr *MyPullRequest) PrepareDir() (string, error) {
-	dirPath := "/var/lib/imbere/" + pr.GetRepoName() + "/" + pr.GetBranchName() + "_" + pr.GetPRID()
+	prIDStr := fmt.Sprintf("%d", int(pr.GetPrNumber()))
+	// dirPath := "/var/lib/imbere/builds/" + pr.GetRepoName() + "/" + pr.GetBranchName() + "_" + prIDStr
+	dirPath := "./builds/" + pr.GetRepoName() + "/" + pr.GetBranchName() + "_" + prIDStr
 
 	// check if dir exists and remove it
 	if _, err := os.Stat(dirPath); !os.IsNotExist(err) {
@@ -291,4 +446,4 @@ func (d *MyDeployment) CommunicateStatus(status string) error {
 // MARK PR CLOSED , NO MORE DEPLOY
 
 // DATABASE TABLE
-// ID, PR(string), BRANCH(string), URL(string), WORKFLOW_SUCCEEDED(1,0), LABELED_TO_DEPLOY(1,0), ACTIVE(1,0), ....INFORMATION FROM PM2
+// ID, PR ID, PR_NUMBER(string), BRANCH(string), URL(string), WORKFLOW_SUCCEEDED(1,0), LABELED_TO_DEPLOY(1,0), ACTIVE(1,0), ....INFORMATION FROM PM2
